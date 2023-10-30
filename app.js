8d2b1fbf272aba42894dc0e2e74bfce6bc034c54
Norbert Ishimwe <ishimwe96@gmail.com>
2023-10-31 00:07:19 +0200

    log .env

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3001;
 
 app.get("/api/", (req, res) => res.type('html').send(html));
-
+console.log(process.env);
 const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
 server.keepAliveTimeout = 120 * 1000;
